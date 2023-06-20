e48f13cbcb71b58a1715939cd67325a8ddaded2f
Akbar <abdullaev.akbar@gmail.com>
2023-06-20 14:58:26 +0300

    Update App.js
    Добавил componentbutton и  коммент

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import { NavBar } from './components/NavBar/NavBar';
+import { ComponentButton } from './components/ComponentButton/ComponentButton';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+    
+      // Здесь мы вызвали кнопку и то что передается "Я кнопка" это по сути как дитя children
+      <ComponentButton variant='contained'>Я Кнопка </ComponentButton>
+    
     </div>
   );
 }
