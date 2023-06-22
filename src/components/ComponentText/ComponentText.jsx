69b7f88daa6360a4b6470a958224cbf0e3c2c556
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-06-22 23:09:59 +0300

    Update ComponentText.jsx

--- src/components/ComponentText/ComponentText.jsx
+++ src/components/ComponentText/ComponentText.jsx
@@ -5,7 +5,7 @@ export const ComponentText = (props) => {
     let { children, ...others } = props
      return (
     <div>
-        <p {...others} style={{ fontSize: 20 }} > 
+        <p {...others} variant={props.variant} > 
             {children}
         </p>
      </div>)
