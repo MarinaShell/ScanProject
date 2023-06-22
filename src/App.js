bd4f8b1746acf94402b1d4057601bc198ffec9f1
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-06-22 21:51:07 +0300

    Update App.js add styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import './components/ComponentText/ComponentText.css';
+import './components/ComponentHeader/ComponentHeader.css';
 import { NavBar } from './components/NavBar/NavBar';
 import { ComponentButton } from './components/ComponentButton/ComponentButton';
 import { ComponentText } from './components/ComponentText/ComponentText';
@@ -7,13 +9,18 @@ import { ComponentHeader } from './components/ComponentHeader/ComponentHeader';
 function App() {
   return (
 	
-    <div className="App">
-		
+    <div className="App">	
       <NavBar/>
-	<ComponentHeader headerTextName="Cервис по поиску публикаций о компании по его ИНН"/>
-      <ComponentText textName="Комплексный анализ публикаций, получение данных в формате PDF на электронную почту."/>
+
+		<div className='size60'>
+		<ComponentHeader headerTextName="Cервис по поиску публикаций о компании по его ИНН"/>
+		</div>
+
+		<div className="size20 colorWhite background">
+      <ComponentText  textName="Комплексный анализ публикаций, получение данных в формате PDF на электронную почту."/>
+		</div>
+
       <ComponentButton variant='contained'>Я Кнопка </ComponentButton>
-    
     </div>
 
   );
