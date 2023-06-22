786b0585acff5173a583dac519e4a9079799ffb4
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-06-22 22:33:51 +0300

    Update ComponentText.jsx

--- src/components/ComponentText/ComponentText.jsx
+++ src/components/ComponentText/ComponentText.jsx
@@ -1,9 +1,12 @@
 import { fontSize } from '@mui/system'
-import React from 'react';
-
+import React from 'react'
 
 export const ComponentText = (props) => {
-	return (<div>
-		<p  > {props.textName}</p>
-	</div>)
+    let { children, ...others } = props
+     return (
+    <div>
+        <p {...others} style={{ fontSize: 20 }} > 
+            {children}
+        </p>
+     </div>)
 }
