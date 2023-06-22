5ab4dbbd2c8cecfd8ab0199cd82432ac0fd3cedb
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-06-22 08:47:28 +0300

    Delete ComponentText directory

--- ComponentText/ComponentText.jsx
+++
@@ -1,8 +0,0 @@
-import { fontSize } from '@mui/system'
-import React from 'react'
-
-export const ComponentText = (props) => {
-	return (<div>
-		<p style={{ fontSize: 20 }} > {props.textName}</p>
-	</div>)
-}
