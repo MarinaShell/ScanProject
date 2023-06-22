e407baec1dbb0e2804e3d814642612760a57f39a
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-06-22 08:48:17 +0300

    Delete ComponentHeader directory

--- ComponentHeader/ComponentHeader.jsx
+++
@@ -1,8 +0,0 @@
-import { fontSize } from '@mui/system'
-import React from 'react'
-
-export const ComponentHeader = (props) => {
-	return (<div>
-		<h1 style={{ fontSize: 60 }} > {props.headerTextName}</h1>
-	</div >)
-}
