690c230b637be97e666e754e76c1b7a4879e9445
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-06-22 08:44:20 +0300

    Update ComponentHeader.jsx

--- src/components/ComponentHeader/ComponentHeader.jsx
+++ src/components/ComponentHeader/ComponentHeader.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export const ComponentHeader = () => {
 	return (<div>
-		<h1 style={{ fontSize: 60 }} > Cервис по поиску публикаций о компании по его ИНН</h1>
+		<h1 style={{ fontSize: 60 }} > {props.headerTextName}</h1>
 	</div >)
 }
