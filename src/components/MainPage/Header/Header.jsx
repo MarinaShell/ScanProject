2c8b5b03650110718bd8718b3b8a6ada05347bf2
Olga Kudryavtseva <105752494+Encephalon1@users.noreply.github.com>
2023-06-23 14:29:45 +0300

    Header complited

--- src/components/MainPage/Header/Header.jsx
+++ src/components/MainPage/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CustomButton } from '../../ComponentButton/ComponentButton'
 import { ComponentHeader } from '../../ComponentHeader/ComponentHeader'
 import { ComponentText } from '../../ComponentText/ComponentText'
 import ComponentImage from '../../ComponentImage/ComponentImage'
@@ -11,12 +12,15 @@ const Header = () => {
     <div className='size60'>
       <ComponentHeader headerTextName='Сервис по поиску публикаций о компании по его ИНН' />
     </div>
-    <div className='size20 colorWhite background'>
+    <div className='size20 colorWhite background' id='text'>
       <ComponentText  textName='Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.'/>
 		</div>
+    <div className='button'>
+    <CustomButton variant='blue'>Запросить данные</CustomButton>
+    </div>
     </div>
     <div className='image'>
-      <ComponentImage source={main_up} width='629px' height='620px' />
+      <ComponentImage source={main_up} width='100%' height='auto' />
     </div>
   </div>
   )
