515feddd2811ee7db676916a5f92070199c74968
newGentle <abdullaev.akbar@gmail.com>
2023-06-24 18:52:46 +0500

    SearchResultSummary added

--- src/components/ResultPage/ResultPage.jsx
+++ src/components/ResultPage/ResultPage.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { CustomContainer } from '../CustomComponents/CustomContainer/CustomContainer';
+import { SearchResultSummary } from './SearchResultSummary';
 
 const ResultPage = () => {
   return (
-    <div>ResultPage</div>
+    <CustomContainer>
+      <SearchResultSummary/>      
+    </CustomContainer>
   )
 }
 
