a29c4c94bd35b52ee19f05f23e8281634f775c2d
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-06-26 14:45:49 +0300

    delete SearchPage

--- src/components/SearchPage/SearchPage.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-// import SearchForm from "./SearchForm/SearchForm";
-// function SearchPage() {
-// 	return (
-// 		<div><SearchForm /></div>
-// 	)
-// }
-
-// export default SearchPage
