a73c18041b7d5151a601cae44dae2ca5ad4f773f
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-06-27 20:09:17 +0300

    Add src

---
+++ src/components/MainPage/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { CustomButton } from '../../CustomComponents/CustomButton/CustomButton'
+import ComponentHeaderText from '../../CustomComponents/ComponentHeaderText/ComponentHeaderText'
+import ComponentText from '../../CustomComponents/ComponentText/ComponentText'
+import ComponentImage from '../../CustomComponents/ComponentImage/ComponentImage'
+import main_up from './main_up.svg'
+import './Header.css'
+
+const Header = () => {
+	return (
+		<div className='header_main'>
+			<div className='left-part'>
+				<div className='left-text'>
+					<div className='size60 size40'>
+						<ComponentHeaderText className="fsz">
+							Сервис по поиску публикаций  <br />о компании <br />по его ИНН
+						</ComponentHeaderText>
+					</div>
+					<div className='size20' id='text'>
+						<ComponentText>
+							Комплексный анализ публикаций, получение данных <br />в формате PDF на электронную почту.
+						</ComponentText>
+					</div>
+				</div>
+				<div className='button'>
+					<CustomButton variant='blue'>Запросить данные</CustomButton>
+				</div>
+			</div >
+			<div className='image'>
+				<ComponentImage source={main_up} width='100%' height='100%' />
+			</div>
+		</div >
+	)
+}
+
+export default Header
---
+++ src/components/SearchPage/SearchForm/SearchForm.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './SearchForm.css';
+import { CustomButton } from '../../CustomComponents/CustomButton/CustomButton'
+
+const SearchForm = () => {
+	return (
+		<div>
+			<div className='form'>
+				<form>
+
+					<div className='part_form1'>
+						<p className='title-input'>ИНН компании*</p>
+						<input className='field-input1' type="number" required="required" placeholder="10 цифр" />
+						<p className='title-input'>Тональность</p>
+						<select className='tonality '>
+							<option value="">Любая</option>
+							<option value="1">Позитивная</option>
+							<option value="2">Негативная</option>
+						</select>
+						<p className='title-input'>Количество документов в выдаче*</p>
+						<input className='field-input1' type="number" required="required" placeholder="от 1 до 1000" />
+
+						<div className='part_form3'>
+							<p className='title-input'>Диапазон поиска*</p>
+							<div className='part_form3_block-input'>
+								<input className='field-input2' name="date" type="text" onfocus="(this.type='date')" onblur="if(!this.value)this.type='text'" required="required" placeholder="Дата начала" />
+								<input className='field-input2' name="date" type="text" onfocus="(this.type='date')" onblur="if(!this.value)this.type='text'" required="required" placeholder="Дата конца" />
+							</div>
+						</div>
+					</div>
+
+
+					<div className="part_form3">
+						<div className='part_form2'>
+							<div className='checkbox'><label><input type="checkbox" className='square' />Признак максимальной полноты</label></div>
+							<div className='checkbox'><label><input type="checkbox" className='square' />Упоминания в бизнес-контенте</label></div>
+							<div className='checkbox'><label><input type="checkbox" className='square' />Главная роль в публикации</label></div>
+							<div className='checkbox'><label><input type="checkbox" className='square' />Публикации только с риск-факторами</label></div>
+							<div className='checkbox'><label><input type="checkbox" className='square' />Включать технические новости рынков</label></div>
+							<div className='checkbox'><label><input type="checkbox" className='square' />Включать анонсы и календари</label></div>
+							<div className='checkbox'><label><input type="checkbox" className='square' />Включать сводки новостей</label></div>
+						</div>
+
+
+						<div className='btn'>
+							<CustomButton variant='blue'>Поиск</CustomButton><br />
+							<p>* Обязательные к заполнению поля</p>
+						</div>
+					</div >
+				</form >
+			</div >
+		</div >
+	)
+}
+
+export default SearchForm
