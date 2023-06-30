6c83a2b8c6b4f3d21b8b1d392d588aeabe89cdf2
Olga Kudryavtseva <105752494+Encephalon1@users.noreply.github.com>
2023-06-30 05:16:45 +0300

    Delete src/components/ComponentHeader directory

--- src/components/ComponentHeader/ComponentHeader.jsx
+++
@@ -1,9 +0,0 @@
-import { fontSize } from '@mui/system'
-import React from 'react'
-import './ComponentHeader.css';
-
-export const ComponentHeader = (props) => {
-	return (<div>
-		<h1 > {props.headerTextName}</h1>
-	</div >)
-}
