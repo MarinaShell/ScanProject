8b59e1578893ea95681d40841be7d23c6bc1719a
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-07-06 19:13:03 +0300

    Update ComponentAuthMediaImages.jsx

--- src/components/AuthPage/ComponentAuthMediaImages/ComponentAuthMediaImages.jsx
+++ src/components/AuthPage/ComponentAuthMediaImages/ComponentAuthMediaImages.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import ComponentImage from '../../CustomComponents/ComponentImage/ComponentImage';
-import MyImage from './images/Characters.svg';
-
-function ComponentAuthMediaImages() {
+import React from "react";
+import ComponentHeaderText from "../../CustomComponents/ComponentHeaderText/ComponentHeaderText";
+import { useTheme, useMediaQuery } from "@mui/material";
+function ComponentAuthMedia() {
+	const theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.down("md"));
 	return (
 		<div>
-			<ComponentImage img source={MyImage} width="322px" height="342px" />
-		</div>
+			<ComponentHeaderText className='font_ferry' style={{
+				fontWeight: "900",
+				marginTop: "20px",
+				textAlign: "left",
+				fontFamily: 'Ferry',
+				fontStyle: "normal",
+				fontWeight: "900",
+				fontSize: matches ? "28px" : "45px",
+				lineHeight: matches ? "33px" : "72px",
+				letterSpacing: "0.02em"
+
+			}}> Для оформления подписки
+				<br />на тариф, необходимо <br />авторизоваться.
+			</ComponentHeaderText>
+		</div >
 	)
 }
 
-export default ComponentAuthMediaImages
+export default ComponentAuthMedia;
