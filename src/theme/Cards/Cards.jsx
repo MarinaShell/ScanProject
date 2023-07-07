8936036f81e641de74f560e6595d5afddf95d38b
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2023-07-07 18:35:23 +0300

    add files

---
+++ src/theme/Cards/Cards.jsx
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    components: {
+        MuiCard: {
+            styleOverrides: {
+                root: {
+                    borderRadius: "10px",
+                    boxShadow: "0px 0px 20px 0px rgba(0, 0, 0, 0.2)",
+                    padding: "20px",
+                },
+            },
+        },
+    },
+});
+
+export { theme };
